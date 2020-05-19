616fd7176617bac019803801bb59413606636126
DR.H2S04 <pratikdai404@gmail.com>
2020-05-19 12:54:51 +0545

    v_1.0.0

---
+++ src/BlogServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace doctype_admin\Blog;
+
+use Illuminate\Support\ServiceProvider;
+
+class BlogServiceProvider extends ServiceProvider
+{
+    /**
+     * Register Doctype Admin Blog Services
+     * 
+     * @return void
+     */
+
+     public function register()
+     {
+         //
+     }
+     /**
+      * Bootstrap Doctype Admin Blog Services
+      *
+      *@return void
+     */
+
+     public function boot()
+     {
+         //
+     }
+}
